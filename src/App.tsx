394c5173cd9c38a88c12eb7a912a6ec4d92bf481
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-29 13:29:46 +0600

    Сделал страницу Quotes, редактирование работает как нужно, работаю над переходами по категориям и удалением цитаты

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import Toolbar from './components/NavBar/NavBar';
+import MutateQuote from './Pages/MutateQuote/MutateQuote';
+import {Route, Routes} from 'react-router-dom';
+import Quotes from './Pages/Quotes/Quotes';
+
 const App = () => (
-    <>
-        da
-    </>
+  <>
+    <header>
+      <Toolbar/>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<Quotes />}/>
+        <Route path="/add-quote" element={<MutateQuote />}/>
+        <Route path="/quotes/:id/edit" element={<MutateQuote/>}/>
+        <Route path="*" element={<h3 className="text-center fs-1">Not Found</h3>}/>
+      </Routes>
+    </main>
+  </>
 );
 
 export default App
